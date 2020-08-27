f1508850e144a3c63ef64c01216ea65684b1976b
toxic-recker <62395124+toxicrecker@users.noreply.github.com>
2020-08-27 17:13:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="DiscordUtils",
     version="1.1.0",
     author="toxicrecker",
-    description="DiscorsUtils is a very useful package for discord.py",
+    description="DiscorsUtils is a very useful library made to be used with discord.py",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://www.github.com/toxicrecker/DiscordUtils",
