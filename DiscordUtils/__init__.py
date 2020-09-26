a76e697f81ad6169bcf60e0945912b6ec6e9ec5a
toxic-recker <62395124+toxicrecker@users.noreply.github.com>
2020-09-26 18:35:07 +0530

    Create __init__.py

---
+++ DiscordUtils/__init__.py
@@ -0,0 +1,7 @@
+from DiscordUtils import Pagination
+from DiscordUtils.InviteTracker import InviteTracker
+
+__title__ = "DiscordUtils"
+__version__ = "1.1.5"
+__author__ = "toxicrecker"
+__license__ = "MIT"
