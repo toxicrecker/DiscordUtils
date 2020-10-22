8c87176102dec3825382e65b7576ae1676e2cffe
toxic-recker <62395124+toxicrecker@users.noreply.github.com>
2020-10-22 11:46:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="DiscordUtils",
-    version="1.1.0",
+    version="1.6",
     author="toxicrecker",
     description="DiscorsUtils is a very useful library made to be used with discord.py",
     long_description=long_description,
