a5e9da2b0241ab74e2f7dfd9fdf79e124c0e055b
toxic-recker <62395124+toxicrecker@users.noreply.github.com>
2020-12-31 13:43:59 +0530

    Update __init__.py

--- DiscordUtils/__init__.py
+++ DiscordUtils/__init__.py
@@ -3,6 +3,6 @@ from DiscordUtils.InviteTracker import InviteTracker
 from DiscordUtils.Music import Music
 
 __title__ = "DiscordUtils"
-__version__ = "1.2.5"
+__version__ = "1.2.6"
 __author__ = "toxicrecker"
 __license__ = "MIT"
