e8ac5d8bb28248f7b5d7b9ff76f7664a7275b435
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-04-21 20:05:21 +0700

    improve __init__

--- DiscordUtils/__init__.py
+++ DiscordUtils/__init__.py
@@ -1,6 +1,6 @@
-from DiscordUtils import Pagination
-from DiscordUtils.InviteTracker import InviteTracker
-from DiscordUtils.Music import Music
+from . import Pagination
+from .InviteTracker import InviteTracker
+from .Music import Music
 
 __title__ = "DiscordUtils"
 __version__ = "1.2.9.1"
