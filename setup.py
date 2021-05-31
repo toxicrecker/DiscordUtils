73d35b26d40ff5a26b04751c04cf81741a84ad83
toxic-recker <62395124+toxicrecker@users.noreply.github.com>
2021-05-31 09:00:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
 
 setuptools.setup(
     name="DiscordUtils",
-    version="1.2.9.2",
+    version="1.3.1",
     author="toxicrecker",
     description="DiscordUtils is a very useful library made to be used with discord.py",
     long_description=long_description,
@@ -17,8 +17,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>= 3.6',
+    python_requires=">= 3.6",
     include_package_data=True,
     install_requires=["discord.py"],
-    extras_require={'voice': ["discord.py[voice]", "youtube-dl"]}
+    extras_require={"voice": ["discord.py[voice]", "youtube-dl"]}
 )
