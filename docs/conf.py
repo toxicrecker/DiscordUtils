1950f86f0f7a23cd204f7ec6480d071a04c0e531
Dhruva Shaw <dhruvashaw@gmail.com>
2022-02-19 20:40:55 +0530

    lets seee

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,6 @@ sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.append(os.path.abspath('extensions'))
-sys.path.insert(1, os.path.dirname(os.path.abspath("../")) + os.sep + "DiscordUtils")
 
 # -- General configuration ------------------------------------------------
 
