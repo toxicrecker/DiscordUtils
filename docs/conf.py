57804095f9736f550008aeace555fb7bfa5eef1f
Dhruva Shaw <dhruvashaw@gmail.com>
2022-02-21 23:21:50 +0530

    reset conf back to normal

--- docs/conf.py
+++ docs/conf.py
@@ -19,11 +19,8 @@ import re
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.dirname(os.path.abspath("../")) + os.sep + "DiscordUtils")
-for x in os.walk('../DiscordUtils'):
-  sys.path.insert(0, x[0])
 sys.path.append(os.path.abspath('extensions'))
 
 # -- General configuration ------------------------------------------------
